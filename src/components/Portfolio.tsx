fac2c37e51c535ac13e17acf50836b79029f3031
Harleym7000 <harleym7000@gmail.com>
2023-07-16 20:46:41 +0100

    Added portfolio section

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,9 +1,75 @@
 import React, {ReactElement} from "react";
+import {Card, Row, Col, Button} from "react-bootstrap";
 
 export default function Portfolio() : ReactElement {
+
+    const pcaHomeImage = require('../pca.PNG');
+
     return (
-        <div className="portfolio mb-5">
-        <h1>Portfolio</h1>
+        <div id="portfolio" className="mb-5">
+            <div className="portfolioContent">
+        <h1 className="portfolioHeading mb-3">Portfolio</h1>
+                <p>Below you can find my portfolio of full-stack web development projects which are currently live.
+                You can click on the GitHub icon to view the code for each project</p>
+            <Row>
+                <Col lg={6}>
+                <Card>
+                    <Card.Img variant="top" src={pcaHomeImage} />
+                    <Card.Body>
+                        <Card.Title>Portstewart Community Association</Card.Title>
+                        <Card.Text>
+                            Portstewart Community Association is a charitable organisation who currently organises and implemets The Red Sails Festival alongside other events and activities for the benefit of the community.
+                        </Card.Text>
+                        <Button
+                            as="a"
+                            onClick={() => window.open("https://www.github.com/Harleym7000", "_blank")}
+                            variant="dark"
+                        >
+                            View code
+                            <i className="bi bi-github" style={{fontSize: "1rem", paddingLeft: 5}}></i>
+                        </Button>
+                        <Button
+                            as="a"
+                            onClick={() => window.open("https://www.portstewartca.org", "_blank")}
+                            style={{marginLeft: 30}}
+                            variant="danger"
+                        >
+                            Visit
+                            <i className="bi bi-box-arrow-up-right" style={{fontSize: "1rem", paddingLeft: 5}}></i>
+                        </Button>
+                    </Card.Body>
+                </Card>
+                </Col>
+                <Col lg={6}>
+                    <Card>
+                        <Card.Img variant="top" src={pcaHomeImage} />
+                        <Card.Body>
+                            <Card.Title>Portstewart Community Association</Card.Title>
+                            <Card.Text>
+                                Portstewart Community Association is a charitable organisation who currently organises and implemets The Red Sails Festival alongside other events and activities for the benefit of the community.
+                            </Card.Text>
+                            <Button
+                                as="a"
+                                onClick={() => window.open("https://www.github.com/Harleym7000", "_blank")}
+                                variant="dark"
+                            >
+                                View code
+                                <i className="bi bi-github" style={{fontSize: "1rem", paddingLeft: 5}}></i>
+                            </Button>
+                            <Button
+                                as="a"
+                                onClick={() => window.open("https://www.github.com/Harleym7000", "_blank")}
+                                style={{marginLeft: 30}}
+                                variant="danger"
+                            >
+                                Visit
+                                <i className="bi bi-box-arrow-up-right" style={{fontSize: "1rem", paddingLeft: 5}}></i>
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
         </div>
     )
 }
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -7,8 +7,9 @@ export default function TechStack() : ReactElement {
     const javascriptLogo = require('../js.png');
 
     return (
-        <div className="techStack pt-5">
+        <div className="techStack">
             <div className="tech pb-5">
+                <h1 className="text-white text-center mt-5 mb-4">What is Full-Stack Development?</h1>
         <Row>
             <CardGroup>
                 <Col sm={12} lg={4}>
@@ -46,38 +47,38 @@ export default function TechStack() : ReactElement {
                     </Col>
             </CardGroup>
         </Row>
-                <h1 className="text-white pt-5 text-center technicalSkillsHeading">My Technical Skills</h1>
+                <h1 className="pt-5 text-center technicalSkillsHeading">My Technical Skills</h1>
         <Row className="mt-4 justify-content-center">
-            <Col lg={1}>
+            <Col lg={2}>
             <Figure>
                 <Figure.Image
                     width={100}
                     alt="React Logo"
                     src={reactLogo}
                 />
-                <Figure.Caption className="text-white text-center">
+                <Figure.Caption className="text-center">
                     React
                 </Figure.Caption>
             </Figure>
             </Col>
-            <Col lg={1}>
+            <Col lg={2}>
             <Figure>
                 <Figure.Image
                     alt="React Logo"
                     src={javascriptLogo}
                 />
-                <Figure.Caption className="text-white text-center">
+                <Figure.Caption className="text-center">
                     Javascript
                 </Figure.Caption>
             </Figure>
             </Col>
-            <Col lg={1}>
+            <Col lg={2}>
             <Figure>
                 <Figure.Image
                     alt="React Logo"
                     src={reactLogo}
                 />
-                <Figure.Caption className="text-white text-center">
+                <Figure.Caption className="text-center">
                     React
                 </Figure.Caption>
             </Figure>
