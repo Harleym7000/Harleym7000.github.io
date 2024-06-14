9de3022fdfc0ba38d4123d13e9107a27cc696769
Harleym7000 <48868940+Harleym7000@users.noreply.github.com>
2024-06-14 12:37:51 +0100

    Reworked design of portfolio and tech stack sections
    Reworked design of portfolio and tech stack sections

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -8,68 +8,33 @@ export default function Portfolio() : ReactElement {
     return (
         <div id="portfolio" className="mb-5">
             <div className="portfolioContent">
-        <h1 className="portfolioHeading mb-3">Portfolio</h1>
+                <h1 className="portfolioHeading mb-3">Portfolio</h1>
                 <p>Below you can find my portfolio of full-stack web development projects which are currently live.
-                You can click on the GitHub icon to view the code for each project</p>
-            <Row>
-                <Col lg={6}>
-                <Card>
-                    <Card.Img variant="top" src={pcaHomeImage} />
-                    <Card.Body>
-                        <Card.Title>Portstewart Community Association</Card.Title>
-                        <Card.Text>
-                            Portstewart Community Association is a charitable organisation who currently organises and implemets The Red Sails Festival alongside other events and activities for the benefit of the community.
-                        </Card.Text>
-                        <Button
-                            as="a"
-                            onClick={() => window.open("https://www.github.com/Harleym7000", "_blank")}
-                            variant="dark"
-                        >
-                            View code
-                            <i className="bi bi-github" style={{fontSize: "1rem", paddingLeft: 5}}></i>
-                        </Button>
-                        <Button
-                            as="a"
-                            onClick={() => window.open("https://www.portstewartca.org", "_blank")}
-                            style={{marginLeft: 30}}
-                            variant="danger"
-                        >
-                            Visit
-                            <i className="bi bi-box-arrow-up-right" style={{fontSize: "1rem", paddingLeft: 5}}></i>
-                        </Button>
-                    </Card.Body>
-                </Card>
-                </Col>
-                <Col lg={6}>
-                    <Card>
-                        <Card.Img variant="top" src={pcaHomeImage} />
-                        <Card.Body>
-                            <Card.Title>Portstewart Community Association</Card.Title>
-                            <Card.Text>
-                                Portstewart Community Association is a charitable organisation who currently organises and implemets The Red Sails Festival alongside other events and activities for the benefit of the community.
-                            </Card.Text>
-                            <Button
-                                as="a"
-                                onClick={() => window.open("https://www.github.com/Harleym7000", "_blank")}
-                                variant="dark"
-                            >
-                                View code
-                                <i className="bi bi-github" style={{fontSize: "1rem", paddingLeft: 5}}></i>
-                            </Button>
-                            <Button
-                                as="a"
-                                onClick={() => window.open("https://www.github.com/Harleym7000", "_blank")}
-                                style={{marginLeft: 30}}
-                                variant="danger"
-                            >
-                                Visit
-                                <i className="bi bi-box-arrow-up-right" style={{fontSize: "1rem", paddingLeft: 5}}></i>
-                            </Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </div>
+                    Feel free to click on visit and have a look around!</p>
+                <Row>
+                    <Col lg={6}>
+                        <Card>
+                            <Card.Img variant="top" src={pcaHomeImage} />
+                            <Card.Body>
+                                <Card.Title>Portstewart Community Association</Card.Title>
+                                <Card.Text>
+                                    Portstewart Community Association is a charitable organisation who currently organises and implemets The Red Sails Festival alongside other events and activities for the benefit of the community.
+                                </Card.Text>
+                                <div className="d-flex flex-row-reverse">
+                                    <Button
+                                        as="a"
+                                        onClick={() => window.open("https://www.portstewartca.org", "_blank")}
+                                        variant="danger"
+                                    >
+                                        Visit
+                                        <i className="bi bi-box-arrow-up-right" style={{fontSize: "1rem", paddingLeft: 5}}></i>
+                                    </Button>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
         </div>
     )
 }
